071cb550a8a5586359e125e725790f92d9740c0c
zelviaolgamaharani <zelviaolgamaharani98@gmail.com>
2020-12-11 14:04:29 +0700

    first

---
+++ app/src/main/java/com/surelabs/news/Config.kt
@@ -0,0 +1,7 @@
+package com.surelabs.news
+
+object Config {
+
+    private const val host = "http://apinews.server4111.com/index.php/"
+
+}
---
+++ app/src/main/java/com/surelabs/news/DetaiNews.kt
@@ -0,0 +1,11 @@
+package com.surelabs.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetaiNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detai_news)
+    }
+}
